c7aa62190301b3bcb7c134b01887d8bdb833c258
SebastienLaguerre <slaguerre2@ufl.edu>
2024-04-10 13:02:43 -0400

    I made my own encoder

---
+++ main.py
@@ -0,0 +1,43 @@
+def menu():
+    print("Menu")
+    print()
+    print("-------------")
+    print()
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+    print()
+
+if __name__ == "__main__":
+    menu()
+    option = int(input("Please enter an option: "))
+    encoded_string = " "
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        for i in password:
+            if i == "0":
+                encoded_string += "3"
+            if i == "1":
+                encoded_string += "4"
+            if i == "2":
+                encoded_string += "5"
+            if i == "3":
+                encoded_string += "6"
+            if i == "4":
+                encoded_string += "7"
+            if i == "5":
+                encoded_string += "8"
+            if i == "6":
+                encoded_string += "9"
+            if i == "7":
+                encoded_string += "0"
+            if i == "8":
+                encoded_string += "1"
+            if i == "9":
+                encoded_string += "2"
+
+
+
+        print("Your password has been encoded and stored!")
+        print(encoded_string)
