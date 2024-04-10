380e677f3cc8fd066ce45e65f8190c31f0835a20
seamusmccol <seam.cco@gmail.com>
2024-04-10 13:24:14 -0400

    seamus added the decode functionality along with the exit functionality

--- main.py
+++ main.py
@@ -9,35 +9,45 @@ def menu():
     print()
     print()
 
-if __name__ == "__main__":
-    menu()
-    option = int(input("Please enter an option: "))
+def main():
     encoded_string = " "
-    if option == 1:
-        password = input("Please enter your password to encode: ")
-        for i in password:
-            if i == "0":
-                encoded_string += "3"
-            if i == "1":
-                encoded_string += "4"
-            if i == "2":
-                encoded_string += "5"
-            if i == "3":
-                encoded_string += "6"
-            if i == "4":
-                encoded_string += "7"
-            if i == "5":
-                encoded_string += "8"
-            if i == "6":
-                encoded_string += "9"
-            if i == "7":
-                encoded_string += "0"
-            if i == "8":
-                encoded_string += "1"
-            if i == "9":
-                encoded_string += "2"
+    while True:
+        menu()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            for i in password:
+                if i == "0":
+                    encoded_string += "3"
+                if i == "1":
+                    encoded_string += "4"
+                if i == "2":
+                    encoded_string += "5"
+                if i == "3":
+                    encoded_string += "6"
+                if i == "4":
+                    encoded_string += "7"
+                if i == "5":
+                    encoded_string += "8"
+                if i == "6":
+                    encoded_string += "9"
+                if i == "7":
+                    encoded_string += "0"
+                if i == "8":
+                    encoded_string += "1"
+                if i == "9":
+                    encoded_string += "2"
+            print(encoded_string)
+            print("Your password has been encoded and stored!")
 
+        if option == 2:
+            #i know how to do this but this is easier lol
+            #seamus added this functionality
+            print(f"The encoded password is {encoded_string}, and the original password is {password}")
+        if option == 3:
+            #seamus also added this functionality
+            break
 
+if __name__ == "__main__":
+    main()
 
-        print("Your password has been encoded and stored!")
-        print(encoded_string)
